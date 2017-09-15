5a369189663d939ad9cb43d7170a7aa5707c1f3b
Dylan Meadows <dmeadows@aamatrix.com>
2017-09-15 16:17:04 -0400

    Committing progress

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,16 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-color-picker',
-  templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+	selector: 'app-color-picker',
+	templateUrl: './color-picker.component.html',
+	styleUrls: ['./color-picker.component.css']
 })
 export class ColorPickerComponent implements OnInit {
 
-  constructor() {
-  }
+	@ViewChild('hue')
+	private _hue: ElementRef;
 
-  ngOnInit() {
-  }
+	@ViewChild('saturation')
+	private _saturation: ElementRef;
+
+	@ViewChild('cursor')
+	private _cursor: ElementRef;
+
+	@ViewChild('luminance')
+	private _luminance: ElementRef;
+
+	@ViewChild('handle')
+	private _handle: ElementRef;
+
+	constructor() {
+	}
+
+	ngOnInit() {
+		const cursor = this._cursor.nativeElement;
+		cursor.addEventListener('mousedown', function (event: MouseEvent) {
+			console.log(event);
+		});
+		const handle = this._handle.nativeElement;
+	}
+
+	log(event) {
+		console.log(event.srcElement.value);
+	}
 
 }
